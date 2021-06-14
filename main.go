c2e99a8967eedb3fe5aa17550039d1419d39b6a8
kalmastenitin <nknckalmaste@gmail.com>
2021-06-14 18:50:43 +0530

    added update and delete methods

--- main.go
+++ main.go
@@ -11,6 +11,7 @@ import (
 	"github.com/go-playground/validator"
 	"github.com/gorilla/mux"
 	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
@@ -66,6 +67,27 @@ func getUsers(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(users)
 }
 
+func getUser(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	var user User
+	var params = mux.Vars(r)
+	id, _ := primitive.ObjectIDFromHex(params["id"])
+	filter := bson.M{"_id": id}
+	err := collection.FindOne(context.TODO(), filter).Decode(&user)
+	if err != nil {
+		var response = ErrorResponse{
+			ErrorMessage: err.Error(),
+			StatusCode:   http.StatusInternalServerError,
+		}
+		// make return json response
+		message, _ := json.Marshal(response)
+		w.WriteHeader(response.StatusCode)
+		w.Write(message)
+		return
+	}
+	json.NewEncoder(w).Encode(user)
+}
+
 func createUser(w http.ResponseWriter, r *http.Request) {
 	// set headers
 	w.Header().Set("Content-Type", "application/json")
@@ -114,6 +136,60 @@ func createUser(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(result)
 }
 
+func updateUser(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	var user User
+	var params = mux.Vars(r)
+	id, _ := primitive.ObjectIDFromHex(params["id"])
+	filter := bson.M{"_id": id}
+	_ = json.NewDecoder(r.Body).Decode(&user)
+
+	update := bson.D{
+		{"$set", bson.D{
+			{"firstname", user.FirstName},
+			{"lastname", user.LastName},
+			{"age", user.Age},
+			{"email", user.Email},
+		}},
+	}
+	fmt.Println(update)
+	err := collection.FindOneAndUpdate(context.TODO(), filter, update).Decode(&user)
+	if err != nil {
+		var response = ErrorResponse{
+			ErrorMessage: err.Error(),
+			StatusCode:   http.StatusInternalServerError,
+		}
+		// make return json response
+		message, _ := json.Marshal(response)
+		w.WriteHeader(response.StatusCode)
+		w.Write(message)
+		return
+	}
+	json.NewEncoder(w).Encode(user)
+}
+
+func deleteUser(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	var params = mux.Vars(r)
+	id, err := primitive.ObjectIDFromHex(params["id"])
+	filter := bson.M{"_id": id}
+	deleteResult, err := collection.DeleteOne(context.TODO(), filter)
+
+	if err != nil {
+		var response = ErrorResponse{
+			ErrorMessage: err.Error(),
+			StatusCode:   http.StatusInternalServerError,
+		}
+		// make return json response
+		message, _ := json.Marshal(response)
+		w.WriteHeader(response.StatusCode)
+		w.Write(message)
+		return
+	}
+	json.NewEncoder(w).Encode(deleteResult)
+
+}
+
 func connectDB() *mongo.Collection {
 	uri := "mongodb://localhost:27017"
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
@@ -123,12 +199,6 @@ func connectDB() *mongo.Collection {
 	if err != nil {
 		panic(err)
 	}
-
-	// defer func() {
-	// 	if err = client.Disconnect(ctx); err != nil {
-	// 		panic(err)
-	// 	}
-	// }()
 	fmt.Println("database is now ready...")
 
 	collection := client.Database("userinfo").Collection("user")
@@ -141,7 +211,9 @@ func main() {
 	validate = validator.New()
 	r := mux.NewRouter()
 	r.HandleFunc("/api/users", getUsers).Methods("GET")
-	// r.HandleFunc("/api/user", getUser).Methods("GET")
+	r.HandleFunc("/api/user/{id}", getUser).Methods("GET")
+	r.HandleFunc("/api/user/{id}", updateUser).Methods("PUT")
+	r.HandleFunc("/api/user/{id}", deleteUser).Methods("DELETE")
 	r.HandleFunc("/api/user", createUser).Methods("POST")
 	log.Fatal(http.ListenAndServe(":8000", r))
 }
